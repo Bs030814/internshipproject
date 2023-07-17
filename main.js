96ee33e03af33801b1cc877b18521127340e60f8
Bs030814 <138363662+Bs030814@users.noreply.github.com>
2023-07-17 15:19:32 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+document.getElementById('loginForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        localStorage.setItem('access_token', data.access_token);
+        window.location.href = '/protected';
+    } else {
+        const messageElement = document.getElementById('message');
+        messageElement.textContent = data.detail;
+    }
+});
